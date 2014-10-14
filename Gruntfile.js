d128c741a611e34ec229a98855c106f2ede8e495
Tanner Welsh <tannerwelsh@gmail.com>
2014-10-14 10:37:26 -0400

    Use picnic.css via bower & grunt-bowercopy
    For this site, bower components will be ignored and copied at build-time
    into their respective directories in the deployable file structure
    
    - Grunt task to copy picnic.css source
    - Ignore bower_components directory
    - Add picnic.css to index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,16 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-bowercopy');
+
+  grunt.initConfig({
+    bowercopy: {
+      css: {
+        options: {
+          destPrefix: 'public/css'
+        }
+      , files: {
+          'picnic.min.css': 'picnic/releases/latest.min.css'
+        }
+      }
+    }
+  });
 };
