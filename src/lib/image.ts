0f7c0b9507c963c2269a8dc658223fc39ed64efe
Tanner Welsh <tannerwelsh@gmail.com>
2024-07-23 13:01:43 -0600

    Fix image basePath

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,3 +1,5 @@
-const basePath = '/'
+// Because github pages deploys to this path, need to include as
+// prefix when fetching images
+const basePath = '/tannerwelsh.github.io'
 
 export const imagePath = (filename: string) => [basePath, filename].join('/')
