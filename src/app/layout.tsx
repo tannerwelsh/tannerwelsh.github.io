cd85f5907127302a2e96b059beb68803e0c31879
Tanner Welsh <tannerwelsh@gmail.com>
2024-07-23 09:39:42 -0600

    Simplify site to single page; replace images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Tanner Welsh',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      "I'm Tanner Welsh, a software engineer & educator living in Colorado.",
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
-  alternates: {
-    types: {
-      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-    },
-  },
+    "I'm Tanner Welsh, a software engineer & educator living in Colorado.",
 }
 
 export default function RootLayout({
